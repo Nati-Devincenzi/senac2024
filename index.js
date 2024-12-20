8f377c14b88fdd7f05abb265c2a91f6578c488eb
Nati-Devincenzi <natidev.senac@gmail.com>
2024-12-20 09:06:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let num1 = Number(prompt("Informe um número: "))
+let num2 = Number(prompt("Informe outro número: "))
+
+if (num1 > num2) {
+    console.log("O primeiro número é maior que o segundo número.")
+} else if (num1 < num2) {
+    console.log("O segundo número é menor que o primeiro número.")
+} else {
+    console.log("Os números são iguais.")
+}
